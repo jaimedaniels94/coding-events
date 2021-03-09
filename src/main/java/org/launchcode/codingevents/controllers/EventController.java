c00e17dd4161bd4937916d2f9f57e0e468a3a9f3
jaimedaniels94 <jaimedaniels94@gmail.com>
2021-03-09 11:13:54 -0600

    added validation

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -1,33 +1,65 @@
 package org.launchcode.codingevents.controllers;
 
 
+import org.launchcode.codingevents.data.EventData;
+import org.launchcode.codingevents.models.Event;
+import org.launchcode.codingevents.models.EventType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.List;
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("events")
 public class EventController {
-
+    //pass in a collection of all events into the template to be displayed
     @GetMapping
     public String displayAllEvents(Model model) {
-        List<String> events = new ArrayList<>();
-        events.add("Code with pride");
-        events.add("Strange Loop");
-        events.add("Apple WWDC");
-        events.add("SpringOne Platform");
-        model.addAttribute("events", events);
+        model.addAttribute("title", "All Events");
+       model.addAttribute("events", EventData.getAll());
         return "events/index";
     }
 
     // lives at /events/create
     @GetMapping("create")
-    public String renderCreateEventForm(){
+    public String displayCreateEventForm (Model model){
+        model.addAttribute("title", "create Event");
+        model.addAttribute(new Event());
+        model.addAttribute("types", EventType.values());
         return "events/create";
+    }
+
+    // lives at /events/create
+    // creates a new event from a form submission
+    @PostMapping("create")
+    public String processCreateEventForm(@ModelAttribute @Valid Event newEvent,
+                                         Errors errors, Model model) {
+        if(errors.hasErrors()) {
+            model.addAttribute("title", "create Event");
+            return "events/create";
+        }
+
+        EventData.add(newEvent);
+        return "redirect:";
+    }
+
+    @GetMapping("delete")
+    public String displayDeleteEventForm(Model model) {
+        model.addAttribute("title", "Delete Events");
+        model.addAttribute("events", EventData.getAll());
+        return "events/delete";
+    }
+
+    @PostMapping("delete")
+    public String processDeleteEventsForm(@RequestParam(required = false) int[] eventIds){
+        if (eventIds!= null) {
+        for (int id : eventIds) {
+            EventData.remove(id);
+        }
+        }
+        return "redirect:";
     }
 
 
